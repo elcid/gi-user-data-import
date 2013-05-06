d27cd1951f52371b6b6e461787795fa071c0bd4d
mart07 <rodrigo.martinez@springer.com>
2013-05-06 17:33:32 +0200

    Helper class for channel connections

---
+++ src/main/java/de/springerprofessional/gi/ftp/Helper.java
@@ -0,0 +1,77 @@
+package de.springerprofessional.gi.ftp;
+
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import org.joda.time.DateTime;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class Helper {
+
+
+    public  static String remoteFileDirectory= "Daten";
+    private static String fileType = "*csv";
+    private static String pattern = "ddMMyyyy";
+    private static DateTime cleanupCutoffdate;
+
+    private static DateTime parseDateFromFileName(String fileName) {
+        Date date = null;
+        try {
+            date = new SimpleDateFormat(pattern, Locale.ENGLISH).parse(fileName);    //TODO filename must be parsed too
+        } catch (ParseException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return new DateTime(date.getTime());
+    }
+
+    public static void clean() throws JSchException {
+        com.jcraft.jsch.ChannelSftp channel = null;
+        try {
+            channel = Helper.openNewTLSftpChannel();
+            channel.connect();
+            channel.cd(remoteFileDirectory);
+
+            List<ChannelSftp.LsEntry> list = channel.ls("*." + fileType);
+            for (ChannelSftp.LsEntry file : list) {
+                String fileName = file.getFilename();
+                DateTime fileDate = new DateTime(parseDateFromFileName(fileName));
+
+                //if this file is older than the cutoff date, delete from the SFTP share
+                if (fileDate.compareTo(cleanupCutoffdate) < 0) {
+                    channel.rm(fileName);
+                }
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        } finally {
+            if (channel != null) {
+                Session session = channel.getSession();
+                channel.disconnect();
+                session.disconnect();
+                System.out.println(channel.isConnected());
+            }
+        }
+    }
+
+    public static ChannelSftp openNewTLSftpChannel() throws JSchException {
+        String HOST = "217.69.92.90";   //gi.de
+        int    PORT = 22;
+        String USER = "sfp";
+        String PASS = "Ahc6eineim";
+
+        JSch jsch = new JSch();
+        Session session = jsch.getSession(USER, HOST, PORT);
+        session.setPassword(PASS);
+        java.util.Properties config = new java.util.Properties();
+        config.put("StrictHostKeyChecking", "no");
+        session.setConfig(config);
+        session.connect();
+        return (ChannelSftp) session.openChannel("sftp");
+    }
+}
--- src/main/java/de/springerprofessional/gi/ftp/SFTPExample.java
+++ src/main/java/de/springerprofessional/gi/ftp/SFTPExample.java
@@ -1,7 +1,6 @@
 package de.springerprofessional.gi.ftp;
 
 import com.jcraft.jsch.ChannelSftp;
-import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.Session;
 
 import java.util.Vector;
@@ -9,31 +8,22 @@ import java.util.Vector;
 public class SFTPExample {
 
     public static void main(String args[]) throws Exception {
-        String HOST = "217.69.92.90";   //gi.de
-        int    PORT = 22;
-        String USER = "sfp";
-        String PASS = "Ahc6eineim";
-        String WORKINGDIR = "Daten";
+
         String destinationPath = "/tmp/";
 
-        JSch jsch = new JSch();
-        Session session = jsch.getSession(USER, HOST, PORT);
-        session.setPassword(PASS);
-        java.util.Properties config = new java.util.Properties();
-        config.put("StrictHostKeyChecking", "no");
-        session.setConfig(config);
-        session.connect();
-        ChannelSftp sftpChannel = (ChannelSftp) session.openChannel("sftp");
+
+        ChannelSftp sftpChannel = Helper.openNewTLSftpChannel();
         sftpChannel.connect();
-        sftpChannel.cd(WORKINGDIR);
+        sftpChannel.cd(Helper.remoteFileDirectory);
 
         Vector<ChannelSftp.LsEntry> list = sftpChannel.ls("*.csv");
         for(ChannelSftp.LsEntry lsEntry: list) {
             System.out.println("Downloading test files");
-            sftpChannel.get(lsEntry.getFilename(), destinationPath + lsEntry.getFilename());
+            final String fileName = lsEntry.getFilename();
+            sftpChannel.get(fileName, destinationPath + fileName);
         }
 
-
+        Session session = sftpChannel.getSession();
         sftpChannel.exit();
         session.disconnect();
     }
