657756bad429a5fc2d6eb6fbb310eb5b1886c9c7
mart07 <rodrigo.martinez@springer.com>
2013-05-07 11:22:43 +0200

    Triggering the csv parser from  Quartz. Application context configured to be accessed from a Quartz Job. For example to access the email beans.

--- src/main/java/de/springerprofessional/gi/scheduling/RunMeJob.java
+++ src/main/java/de/springerprofessional/gi/scheduling/RunMeJob.java
@@ -2,19 +2,35 @@ package de.springerprofessional.gi.scheduling;
 
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 public class RunMeJob extends QuartzJobBean {
     private RunMeTask runMeTask;
+    private static final String APPLICATION_CONTEXT_KEY = "applicationContext";
 
     public void setRunMeTask(RunMeTask runMeTask) {
         this.runMeTask = runMeTask;
     }
 
-    protected void executeInternal(JobExecutionContext context)
-            throws JobExecutionException {
-
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+        try {
+            ApplicationContext appCtx = getApplicationContext(context);
+        } catch (Exception e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
         runMeTask.printMe();
 
+    }
+
+    private ApplicationContext getApplicationContext(JobExecutionContext context)
+            throws Exception {
+        ApplicationContext appCtx;
+        appCtx = (ApplicationContext) context.getScheduler().getContext().get(APPLICATION_CONTEXT_KEY);
+        if (appCtx == null) {
+            throw new JobExecutionException(
+                    "No application context available in scheduler context for key \"" + APPLICATION_CONTEXT_KEY + "\"");
+        }
+        return appCtx;
     }
 }
